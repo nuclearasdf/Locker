765379a7764e0656c1afd654d467e63184f9abdf
HanJongSu <gkswhd8212@naver.com>
2016-11-24 16:45:26 +0900

    Update PurchaseController.java

--- src/com/virus/PurchaseController.java
+++ src/com/virus/PurchaseController.java
@@ -3,15 +3,10 @@ package com.virus;
 import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-//import javafx.fxml.FXMLLoader;
-//import javafx.scene.Cursor;
 import javafx.scene.Parent;
-//import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
-//import javafx.scene.control.Hyperlink;
 import javafx.scene.control.TextField;
-//import javafx.stage.Stage;
 
 import java.io.IOException;
 
@@ -48,15 +43,6 @@ public class PurchaseController {
                 alert.setContentText("Recovery code has been confirmed. Starting a decrypted.");
                 alert.showAndWait();
 
-				/*
-                Scene scene;
-                Scene thisScene = root.getScene();
-                Stage stage = (Stage) thisScene.getWindow();
-
-                scene = new Scene(FXMLLoader.load(getClass().getResource("Success.fxml")));
-
-                thisScene.setCursor(Cursor.WAIT); 전부 지움
-				*/
 
                 Task<Void> task = new Task<Void>() {
                     @Override
